5c93b26406978997d0979b205a5fbfe4c747f74a
jcservices@yeah.net <jcservices@yeah.net>
2018-07-23 09:54:37 +0800

    -

--- src/com/action/HelloControl.java
+++ src/com/action/HelloControl.java
@@ -10,7 +10,7 @@ public class HelloControl {
 	@RequestMapping(value = "/byyy")
 	public String hello(Model model) { // user:视图层传给控制层的表单对象；model：控制层返回给视图层的对象
 		model.addAttribute("user", "123");
-		
+		System.out.println("本地修改上传");
 		return "hello";
 	}
 }
