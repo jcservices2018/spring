c14f00dda833d247dadd43c5b2e65f76ad8f3a36
jcservices@yeah.net <jcservices@yeah.net>
2018-07-23 10:31:45 +0800

    -

--- src/com/listener/Log4jListener.java
+++ src/com/listener/Log4jListener.java
@@ -2,14 +2,15 @@ package com.listener;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Properties;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.PropertyConfigurator;
+
+import com.util.LoggerUtils;
 
 public class Log4jListener implements ServletContextListener {
 
@@ -20,18 +21,15 @@ public class Log4jListener implements ServletContextListener {
 	@Override
 	public void contextInitialized(ServletContextEvent arg0) {
 		String rootPath = arg0.getServletContext().getRealPath("/");
-		System.out.println(rootPath);
 		System.setProperty("web.root", rootPath);
-		Properties properties = new Properties();
 		try {
+			Properties properties = new Properties();
 			properties.load(new FileInputStream(rootPath+"WEB-INF"+File.separator+"classes"+File.separator+"log4j.properties"));
-			System.out.println("获取key值: "+properties.get("log4j.rootLogger"));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+			PropertyConfigurator.configure(properties);
+		} catch (Exception e) {
+			BasicConfigurator.configure();
+			LoggerUtils.error(e);
 		}
-		PropertyConfigurator.configure(properties);
 	}
 
 }
